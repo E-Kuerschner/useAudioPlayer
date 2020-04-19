6f3f33777036309eb85854abef176cf7b347aceb
Erich Kuerschner <erichk@vailsys.com>
2020-04-19 18:26:04 -0500

    Implement custom seek function to ensure consistent return type

--- src/useAudioPlayer.ts
+++ src/useAudioPlayer.ts
@@ -9,8 +9,8 @@ export type AudioPlayerControls = Omit<AudioPlayer, "player"> & {
     pause: Howl["pause"] | typeof noop
     stop: Howl["stop"] | typeof noop
     mute: Howl["mute"] | typeof noop
-    seek: Howl["seek"] | typeof noop
     volume: Howl["volume"] | typeof noop
+    seek: (position?: number) => number | undefined
     togglePlayPause: () => void
 }
 
@@ -37,14 +37,30 @@ export const useAudioPlayer = (props?: AudioSrcProps): AudioPlayerControls => {
         }
     }, [player])
 
+    const seek: AudioPlayerControls["seek"] = useCallback(
+        position => {
+            const isHowl = (input: any | Howl): input is Howl => {
+                if (input._src) return true
+                return false
+            }
+
+            if (!player) return
+
+            const result = player.seek(position)
+            if (isHowl(result)) return
+            return result
+        },
+        [player]
+    )
+
     return {
         ...context,
         play: player ? player.play.bind(player) : noop,
         pause: player ? player.pause.bind(player) : noop,
         stop: player ? player.stop.bind(player) : noop,
         mute: player ? player.mute.bind(player) : noop,
-        seek: player ? player.seek.bind(player) : noop,
         volume: player ? player.volume.bind(player) : noop,
+        seek,
         load,
         togglePlayPause
     }
